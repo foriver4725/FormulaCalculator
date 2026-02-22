5381acff57da325afb2bbaa075a26e752f392af7
foriver4725 <hrt67@icloud.com>
2026-02-22 16:20:22 +0900

    プロファイリング改善

--- Assets/foriver4725/Profiling/GameManager.cs
+++ Assets/foriver4725/Profiling/GameManager.cs
@@ -1,35 +1,69 @@
 using System;
-using UnityEditor;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Profiling;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace foriver4725.FormulaCalculator.Profiling
 {
     internal sealed class GameManager : MonoBehaviour
     {
-        [SerializeField] private bool doSkipValidation;
+        [SerializeField] private TMP_InputField formulaInputField;
+        [SerializeField] private Slider loopAmountSlider;
+        [SerializeField] private TextMeshProUGUI loopAmountText;
+        [SerializeField] private Toggle doSkipValidationToggle;
+        [SerializeField] private TMP_InputField profilerLabelInputField;
 
-        private const string ProfilerName = "### FormulaCalculator.Calculate() ###";
-        private const string Formula = "1+2*3/(4-5)";
-        private const ulong LoopAmount = 1_000_000;
+        private void Awake()
+        {
+            formulaInputField.text = "1+2*3/(4-5)";
+            loopAmountSlider.onValueChanged.AddListener(value =>
+                loopAmountText.text = "Loop Amount : " + value.ToString("#,0"));
+            loopAmountSlider.value = 1_000_000;
+            doSkipValidationToggle.isOn = false;
+            profilerLabelInputField.text = "### FormulaCalculator.Calculate() ###";
+        }
 
         private void Update()
         {
-            if (Keyboard.current.spaceKey.wasPressedThisFrame)
+            try
             {
-                // Warmup
-                for (int i = 0; i < 8; i++)
-                    _ = Formula.AsSpan().Calculate(doSkipValidation: doSkipValidation);
-
-                UnityEngine.Profiling.Profiler.BeginSample(ProfilerName);
-
-                for (ulong i = 0; i < LoopAmount; i++)
-                    _ = Formula.AsSpan().Calculate(doSkipValidation: doSkipValidation);
-
-                UnityEngine.Profiling.Profiler.EndSample();
-
-                EditorApplication.isPlaying = false;
+                if (Keyboard.current.spaceKey.wasPressedThisFrame)
+                {
+                    Run(
+                        formulaInputField.text.AsSpan(),
+                        (ulong)loopAmountSlider.value,
+                        doSkipValidationToggle.isOn,
+                        profilerLabelInputField.text
+                    );
+                }
+                else if (Keyboard.current.escapeKey.wasPressedThisFrame)
+                {
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+                }
             }
+            catch (Exception e)
+            {
+                Debug.LogError(e);
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+
+        private static void Run(ReadOnlySpan<char> formula, ulong loopAmount, bool doSkipValidation,
+            string profilerLabel)
+        {
+            // Warmup
+            for (int i = 0; i < 8; i++)
+                _ = formula.Calculate(doSkipValidation: doSkipValidation);
+
+            Profiler.BeginSample(profilerLabel);
+
+            for (ulong i = 0; i < loopAmount; i++)
+                _ = formula.Calculate(doSkipValidation: doSkipValidation);
+
+            Profiler.EndSample();
         }
     }
 }
